18c3813baffa2fc8d5991ee2fd58dfc3c86472c6
goncalveshigino <goncalveshigino20@gmail.com>
2023-09-25 21:26:03 +0100

    Adicionar onTapGesture

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -24,23 +24,24 @@ struct ContentView: View {
 
 struct CardView: View {
     
-    var isFaceUp: Bool = false
+    @State var isFaceUp: Bool = false
     
     var body: some View {
-        ZStack( alignment: .top, content: {
+        ZStack {
+            
+            let base = RoundedRectangle(cornerRadius: 12)
+            
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                
-                RoundedRectangle(cornerRadius: 12)
-                    //.strokeBorder(style: StrokeStyle(lineWidth: 10, dash: [10,2]))
-                    .strokeBorder(lineWidth: 2)
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
                 Text("🤗").font(.largeTitle)
             } else {
-                RoundedRectangle(cornerRadius: 12)
+                base.fill()
             }
-          
-        })
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
     }
 }
 

86ed118f2204a11369260fc14fddd809df6224e1
goncalveshigino <goncalveshigino20@gmail.com>
2023-09-25 20:53:46 +0100

    feat:App Memorize

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,19 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+        
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+          CardView( isFaceUp: true )
+          CardView()
+          CardView()
+          CardView()
         }
+     
+        .foregroundColor(.orange)
         .padding()
     }
 }
+
+struct CardView: View {
+    
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack( alignment: .top, content: {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                
+                RoundedRectangle(cornerRadius: 12)
+                    //.strokeBorder(style: StrokeStyle(lineWidth: 10, dash: [10,2]))
+                    .strokeBorder(lineWidth: 2)
+                Text("🤗").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+            }
+          
+        })
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
